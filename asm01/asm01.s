b0bf6c915bb587c4ced9ba13d3c5db2730f91547
ValentineLdre <valentineldr@outlook.fr>
2026-09-04 17:53:38 +0200

    asm01

---
+++ asm01/asm01.s
@@ -0,0 +1,17 @@
+section .data
+number db "1337"
+
+section .text
+global _start
+
+_start:
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, number
+    mov rdx, 4
+    syscall
+    
+    mov rax, 60
+    mov rdi, 0
+    syscall
