1598fa356af4f8aebfaadeab4d38862a9a297e6f
ValentineLdre <valentineldr@outlook.fr>
2026-09-07 16:12:22 +0200

    asm04

--- asm05/asm05.s
+++ asm05/asm05.s
@@ -5,17 +5,11 @@ section .text
 global _start
 
 _start:
-
-    mov rax, 0
-    mov rdi, 0
-    mov rsi, saisie_texte
-    mov rdx, 100
-    syscall
     
     mov rax, 1
     mov rdi, 1
     mov rsi, saisie_texte
-    mov rdx, 100
+    mov rdx, 17
     syscall
     
     mov rax, 60
