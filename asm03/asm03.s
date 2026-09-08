5cb940c33da7534bd6e27b5289d99b3fabedaaee
ValentineLdre <valentineldr@outlook.fr>
2026-09-08 18:22:38 +0200

    asm03

--- asm03/asm03.s
+++ asm03/asm03.s
@@ -1,11 +1,9 @@
 section .data
-number db "1337"
+number db "1337", 10
 
 section .text
 global _start
-
 _start:
-
     mov rsi, [rsp + 16]
 
     mov al, [rsi]
@@ -16,12 +14,16 @@ _start:
     cmp al, '2'
     jne erreur
 
+    mov al, [rsi + 2]
+    cmp al, 0
+    jne erreur
+
     mov rax, 1
     mov rdi, 1
     mov rsi, number
-    mov rdx, 4
+    mov rdx, 5
     syscall
-    
+
     mov rax, 60
     mov rdi, 0
     syscall
