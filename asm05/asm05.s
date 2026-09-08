747fc31c2f91a97a04a0f6e0d20d867653b0325b
ValentineLdre <valentineldr@outlook.fr>
2026-09-08 14:54:39 +0200

    asm05

--- asm05/asm05.s
+++ asm05/asm05.s
@@ -1,17 +1,25 @@
-section .data
-    saisie_texte db "Hello, Universe!"
-
 section .text
 global _start
 
+
 _start:
+    mov rsi, [rsp + 16]
+    mov rcx, 0
+
     
+boucle_longueur:
+    cmp byte [rsi+rcx], 0
+    je  fin_longueur
+    inc rcx
+    jmp boucle_longueur
+
+    
+fin_longueur:
     mov rax, 1
     mov rdi, 1
-    mov rsi, saisie_texte
-    mov rdx, 17
+    mov rdx, rcx
     syscall
-    
+
     mov rax, 60
     mov rdi, 0
     syscall
