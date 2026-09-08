59bb20ca6696dea9494fe570b356c730f2b202c7
ValentineLdre <valentineldr@outlook.fr>
2026-09-08 18:39:58 +0200

    asm04

--- asm04/asm04.s
+++ asm04/asm04.s
@@ -1,71 +1,52 @@
 section .bss
-    saisie_utilisateur resb 8
+    buffer resb 2
 
 section .text
 global _start
-
 _start:
-
     mov rax, 0
     mov rdi, 0
-    mov rsi, saisie_utilisateur
-    mov rdx, 8
+    mov rsi, buffer
+    mov rdx, 2
     syscall
 
+    mov rsi, buffer
     mov al, [rsi]
-    cmp al, '1'
+
+    cmp al, '0'
     jb  not_a_number
     cmp al, '9'
     ja  not_a_number
 
-    mov al, [rsi]
+    cmp al, '0'
+    je even
     cmp al, '2'
     je even
-
-    mov al, [rsi]
     cmp al, '4'
     je even
-
-    mov al, [rsi]
     cmp al, '6'
     je even
-
-    mov al, [rsi]
     cmp al, '8'
     je even
-
-    mov al, [rsi]
     cmp al, '1'
     je odd
-
-    mov al, [rsi]
     cmp al, '3'
     je odd
-
-    mov al, [rsi]
     cmp al, '5'
     je odd
-
-    mov al, [rsi]
     cmp al, '7'
     je odd
-
-    mov al, [rsi]
     cmp al, '9'
     je odd
-    
+
 even:
     mov rax, 60
     mov rdi, 0
     syscall
-
-
 odd:
     mov rax, 60
     mov rdi, 1
     syscall
-
-
 not_a_number:
     mov rax, 60
     mov rdi, 2
